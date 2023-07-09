4a81787e43445810a003d02072995e46db989c92
Jess Dam <jess.dam@starlingbank.com>
2023-07-09 12:05:07 +0100

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const PROMPTS: string[] = []
+const ANSWERS: string[] = []
+
+
+function Start(props: { }) {
+  return <>
+    <h1>Cards Against Socials</h1>
+    <div className='instructions'>
+      <p>
+        In this game you will get a prompt card that will be the beginning of a sentence, and three answer cards 
+        that tell you the end of a sentence. Your job is to combine the prompt card with an answer card to come up with a funnier sentence than your opposition to win a round! 
+        <br></br>
+        <br></br>
+        Press Start to begin.
+      </p>
+    </div>
+    <button>Start</button>
+  </>
+}
+
+function Game(props: { }) {
+  return <>
+    <h1>Cards Against Socials</h1>
+    <div>
+      <Prompt /> 
+      <Answer />
+    </div>
+  </>
+}
+
+function Prompt () {
+  return <>
+    <h4>Prompt</h4>
+    <div className='card-container'>
+      <Card message='Hello my name is ' isPromptCard={true} />
+    </div>
+  </>
+}
+
+function Answer () {
+  return <>
+    <h4>Answer</h4>
+    <p>Pick your answer:</p>
+    <div className='card-container'>
+      <Card message='Wut' isPromptCard={false} />
+      <Card message='Wut' isPromptCard={false} />
+      <Card message='Wut' isPromptCard={false} />
+    </div>
+  </>
+}
+
+function Card(props: { message: string, isPromptCard: boolean }) {
+  let cardStyleType: string = props.isPromptCard ? 'prompt-card' : 'answer-card'
+
+  return <div className={cardStyleType}>
+    <p>{props.message}</p>
+  </div>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="">
       </header>
+      <div className="App-content">
+        {/* <Start /> */}
+        <Game />
+      </div>
     </div>
   );
 }
